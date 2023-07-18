28a5c3f491278535799d933caf3d6debfbac4494
jjiatz <jjiatzg@miumg.edu.gt>
2023-07-18 16:33:58 -0600

    18jul com126

--- cmp/views.py
+++ cmp/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.views import generic
 from django.urls import reverse_lazy
+import datetime
 
-from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponse
 import json
 
@@ -10,9 +10,9 @@ from django.contrib.messages.views import SuccessMessageMixin
 from django.contrib.auth.decorators import login_required, permission_required
 
 from .models import Proveedor, ComprasEnc, CompraDet
-from cmp.forms import ProveedorForm
-
+from cmp.forms import ProveedorForm, ComprasEncForm
 from index.views import sinprivilegios
+from inv.models import Producto
 
 class ProveedorView(sinprivilegios, generic.ListView):
     model = Proveedor
@@ -72,3 +72,36 @@ class ComprasView(sinprivilegios, generic.ListView):
     template_name = "cmp/compras_list.html"
     context_object_name = "obj"
     permission_required="cmp.view_comprasenc"
+
+@login_required(login_url='/login/')
+@permission_required('cmp.view_comprasenc', login_url='bases:sin_privilegios')
+def compras(request, compra_id=None):
+    template_name="cmp/compras.html"
+    prod=Producto.objects.filter(estado=True)
+    form_compras={}
+    contexto={}
+
+    if request.method=='GET':
+        form_compras=ComprasEncForm()
+        enc = ComprasEnc.objects.filter(pk=compra_id).first()
+
+        if enc:
+            det = CompraDet.objects.filter(compras=enc)
+            fecha_compra = datetime.date.isoformat(enc.fecha_compra)
+            fecha_factura = datetime.date.isoformat(enc.fecha_factura)
+            e= {
+                'fecha_compra':fecha_compra,
+                'proveedor':enc.proveedor,
+                'observacion':enc.observacion,
+                'no_factura':fecha_factura,
+                'fecha_factura':fecha_factura,
+                'sub_total':enc.sub_total,
+                'descuento':enc.descuento,
+                'total':enc.total
+            }
+            form_compras = ComprasEncForm(e)
+        else:
+            det=None
+        contexto={'producto':prod,'encabezado':enc,'detalle':det,'form_enc':form_compras}
+
+        return render(request, template_name, contexto)

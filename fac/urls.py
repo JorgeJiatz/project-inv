80ea73586df8c6176c57166fa95b694f49389799
jjiatz <jjiatzg@miumg.edu.gt>
2023-08-25 00:46:28 -0600

    25/08/2023 impFac

--- fac/urls.py
+++ fac/urls.py
@@ -3,6 +3,8 @@ from django.urls import path, include
 from .views import ClienteView, ClienteEdit, ClienteNew, clienteInactivar, \
     FacturaView, facturas, ProductView, del_detalle_factura
 
+from .reportes import imprimir_factura_recibo
+
 urlpatterns = [
     path('clientes/',ClienteView.as_view(), name="cliente_list"),
     path('clientes/new',ClienteNew.as_view(), name="cliente_new"),
@@ -12,5 +14,7 @@ urlpatterns = [
     path('facturas/new',facturas, name="factura_new"),
     path('facturas/edit/<int:id>',facturas, name="factura_edit"),
     path('facturas/buscar-product',ProductView.as_view(), name="fac_product"),
-    path('facturas/del_detalle_factura/<int:id>',del_detalle_factura, name="del_detalle_factura")
+    path('facturas/del_detalle_factura/<int:id>',del_detalle_factura, name="del_detalle_factura"),
+    path('facturas/imprimir/<int:id>',imprimir_factura_recibo, name="factura_imprimir_one")
+    
 ]
